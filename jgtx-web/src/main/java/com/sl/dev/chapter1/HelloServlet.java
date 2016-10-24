59dc9c1e70398c8bdc7b460d10fa69a01729ff7e
unknown <lisulong@lisulong.zaonline.com>
2016-10-24 13:03:39 +0800

    init

---
+++ jgtx-web/src/main/java/com/sl/dev/chapter1/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.sl.dev.chapter1;
+
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.util.Date;
+
+/**
+ * Created by lisulong on 2016/10/23.
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dataFormat.format(new Date());
+        req.setAttribute("currentTime", currentTime+"时间123");
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+}
